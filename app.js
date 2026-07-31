49c0dc4475c1b4014c8aabf0ed7a46bc9dc96acc
homecare037-lgtm <homecare037@gmail.com>
2026-07-31 10:04:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+import {
+  db,
+  collection,
+  getDocs
+} from "./firebase.js";
 
+const testsDiv = document.getElementById("testsList");
+
+async function loadTests() {
+
+    const snapshot = await getDocs(collection(db, "tests"));
+
+    let html = "";
+
+    snapshot.forEach((doc) => {
+
+        const test = doc.data();
+
+        html += `
+<label>
+<input type="checkbox"
+value="${test.name}"
+data-price="${test.price}"
+onchange="calculateTotal()">
+
+${test.name} — ₹${test.price}
+
+</label><br>
+`;
+
+    });
+
+    testsDiv.innerHTML = html;
+
+}
+
+window.loadTests = loadTests;
+
+loadTests();
